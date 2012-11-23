8613439bfa6dd305c78b3f47463f3397e9ca264e
Akira TAGOH <akira@tagoh.org>
2012-11-23 15:30:21 +0900

    correct FSF address in the source code

--- libimchooseui/imchooseui.h
+++ libimchooseui/imchooseui.h
@@ -1,7 +1,7 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /* 
  * imchooseui.h
- * Copyright (C) 2007-2011 Red Hat, Inc. All rights reserved.
+ * Copyright (C) 2007-2012 Red Hat, Inc. All rights reserved.
  * 
  * Authors:
  *   Akira TAGOH  <tagoh@redhat.com>
@@ -18,8 +18,8 @@
  * 
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA  02110-1301  USA.
  */
 #ifndef __IM_CHOOSE_UI_H__
 #define __IM_CHOOSE_UI_H__
